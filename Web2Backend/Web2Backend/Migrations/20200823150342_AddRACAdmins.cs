78a72603d5e6a1f35845841958b57f2ffd059203
Angelina <angelina.gina2@gmail.com>
2020-08-23 17:14:29 +0200

    Vehicle, RAC (kompanija i admin) backend

---
+++ Web2Backend/Web2Backend/Migrations/20200823150342_AddRACAdmins.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web2Backend.Migrations
+{
+    public partial class AddRACAdmins : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RACAdmins",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false),
+                    RACID = table.Column<int>(nullable: true),
+                    Username = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RACAdmins", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_RACAdmins_RACServices_RACID",
+                        column: x => x.RACID,
+                        principalTable: "RACServices",
+                        principalColumn: "RACID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RACAdmins_RACID",
+                table: "RACAdmins",
+                column: "RACID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RACAdmins");
+        }
+    }
+}
---
+++ Web2Backend/Web2Backend/Model/RACService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web2Backend.Model
+{
+    public class RACService
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int RACID { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        //public string[] Branches { get; set; }
+        public virtual ICollection<Vehicle> Vehicles { get; set; }
+        public virtual ICollection<RACAdmin> RACAdmins { get; set; }
+
+        public RACService()
+        {
+            this.Vehicles = new HashSet<Vehicle>();
+            this.RACAdmins = new HashSet<RACAdmin>();
+        }
+
+
+    }
+}
